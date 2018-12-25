403bbf0bfe30b6e07bb6c0913189bb3f06d24ba0
Zeyu Chen <zeyuchen@outlook.com>
2018-12-26 00:31:19 +0800

    add module

---
+++ paddle_hub/module.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import paddle.fluid as fluid
+
+
+class Module(object):
+    def __init__(self, spec):
+        pass
