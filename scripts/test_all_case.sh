f42505c0ae2a80af4ddbea834486407f9c2b92af
wuzewu <wuzewu@baidu.com>
2019-01-07 17:50:00 +0800

    update the test case run script to support specify a test case list file

--- scripts/test_all_case.sh
+++ scripts/test_all_case.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 set -o errexit
 
+function usage() {
+    echo "usage: sh $0 {test_case_list_file}"
+}
+
+if [ $# -lt 1 ]
+then
+    usage
+    exit 1
+fi
+
+listfile=$1
 base_path=$(cd `dirname $0`/..; pwd)
 test_case_path=${base_path}/tests
-
 export PYTHONPATH=$base_path:$PYTHONPATH
+
+# install the require package
 cd ${base_path}
 pip install -r requirements.txt
-cd ${test_case_path}
 
-# only run python test case
-for test_file in `ls | grep \.py`
+# run all case list in the {listfile}
+cd ${test_case_path}
+for test_file in `cat $listfile`
 do
-	python $test_file
+	python ${test_file}.py
 done
