b42d9ab221f95a51fdd77e433ce2cbe1990165a2
Zeyu Chen <chenzeyu01@baidu.com>
2019-04-04 13:56:08 +0800

    update typo

--- demo/ernie-seq-labeling/finetune_with_hub.py
+++ demo/ernie-seq-labeling/finetune_with_hub.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Finetuning on classification tasks."""
+"""Finetuning on sequence labeling task."""
 
 from __future__ import absolute_import
 from __future__ import division
