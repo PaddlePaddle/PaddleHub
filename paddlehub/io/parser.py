0b42516ff877aede1c89525b065bea6794bba891
wuzewu <wuzewu@baidu.com>
2019-04-10 19:06:33 +0800

    fix yaml warning

--- paddlehub/io/parser.py
+++ paddlehub/io/parser.py
@@ -52,7 +52,7 @@ class YAMLFileParser:
     def parse(self, yaml_file):
         with open(yaml_file, "r") as file:
             content = file.read()
-        return yaml.load(content)
+        return yaml.load(content, Loader=yaml.BaseLoader)
 
 
 class TextFileParser:
