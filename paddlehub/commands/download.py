fe7f910122bc622c10b8564a622a2f0c80329760
zhangxuefei <zhangxuefei@baidu.com>
2019-04-15 12:42:15 +0800

    Fix the bug that user can't download the modules

--- paddlehub/commands/download.py
+++ paddlehub/commands/download.py
@@ -31,45 +31,50 @@ class DownloadCommand(BaseCommand):
     def __init__(self, name):
         super(DownloadCommand, self).__init__(name)
         self.show_in_help = True
-        self.description = "Download PaddlePaddle pretrained model files."
+        self.description = "Download PaddlePaddle pretrained model/module files."
         self.parser = self.parser = argparse.ArgumentParser(
             description=self.__class__.__doc__,
-            prog='%s %s <model_name>' % (ENTRY, name),
+            prog='%s %s <model_name/module_name>' % (ENTRY, name),
             usage='%(prog)s [options]',
             add_help=False)
         # yapf: disable
-        self.add_arg('--output_path',  str,  ".",   "path to save the model" )
+        self.add_arg("--type",         str,  "All", "choice: Module/Model/All")
+        self.add_arg('--output_path',  str,  ".",   "path to save the model/module" )
         self.add_arg('--uncompress',   bool, False,  "uncompress the download package or not" )
         # yapf: enable
 
     def exec(self, argv):
         if not argv:
-            print("ERROR: Please provide the model name\n")
+            print("ERROR: Please provide the model/module name\n")
             self.help()
             return False
-        model_name = argv[0]
-        model_version = None if "==" not in model_name else model_name.split(
-            "==")[1]
-        model_name = model_name if "==" not in model_name else model_name.split(
-            "==")[0]
+        mod_name = argv[0]
+        mod_version = None if "==" not in mod_name else mod_name.split("==")[1]
+        mod_name = mod_name if "==" not in mod_name else mod_name.split("==")[0]
         self.args = self.parser.parse_args(argv[1:])
-        if not self.args.output_path:
-            self.args.output_path = "."
-        utils.check_path(self.args.output_path)
+        self.args.type = self.check_type(self.args.type)
 
-        search_result = default_hub_server.get_model_url(
-            model_name, version=model_version)
+        if self.args.type in ["Module", "Model"]:
+            search_result = default_hub_server.get_resource_url(
+                mod_name, resource_type=self.args.type, version=mod_version)
+        else:
+            search_result = default_hub_server.get_resource_url(
+                mod_name, resource_type="Module", version=mod_version)
+            if search_result == {}:
+                search_result = default_hub_server.get_resource_url(
+                    mod_name, resource_type="Model", version=mod_version)
         url = search_result.get('url', None)
         except_md5_value = search_result.get('md5', None)
         if not url:
-            tips = "Can't found model %s" % model_name
+            tips = "Can't found model/module %s" % mod_name
             if model_version:
-                tips += " with version %s" % model_version
+                tips += " with version %s" % mod_version
             print(tips)
             return True
 
         need_to_download_file = True
         file_name = os.path.basename(url)
+        utils.check_path(self.args.output_path)
         file = os.path.join(self.args.output_path, file_name)
         if os.path.exists(file):
             print("File %s already existed\nWait to check the MD5 value" %
@@ -97,6 +102,16 @@ class DownloadCommand(BaseCommand):
                 print_progress=True)
             print(tips)
         return True
+
+    def check_type(self, type):
+        type = type.lower()
+        if type == "module":
+            type = "Module"
+        elif type == "model":
+            type = "Model"
+        else:
+            type = "All"
+        return type
 
 
 command = DownloadCommand.instance()
