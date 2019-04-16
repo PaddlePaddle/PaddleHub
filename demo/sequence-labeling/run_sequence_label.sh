0d987a6ff3680fe9c47c3c598387daecc2da1699
wuzewu <wuzewu@baidu.com>
2019-04-16 10:29:47 +0800

    update demo script

--- demo/sequence-labeling/run_sequence_label.sh
+++ demo/sequence-labeling/run_sequence_label.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=0
 
 CKPT_DIR="./ckpt_sequence_label"
 python -u sequence_label.py \
--- demo/text-classification/run_classifier.sh
+++ demo/text-classification/run_classifier.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=5
+export CUDA_VISIBLE_DEVICES=0
 
 # User can select chnsenticorp, nlpcc_dbqa, lcqmc for different task
 DATASET="chnsenticorp"
