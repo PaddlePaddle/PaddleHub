0a51afd8c70dcb45793624b3667542bab40a869a
zhangxuefei <zhangxuefei@baidu.com>
2019-04-18 17:00:11 +0800

    Update the PaddleHub show name

--- paddlehub/__init__.py
+++ paddlehub/__init__.py
@@ -11,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+HUB_SHOW_NAME = "PaddleHub"
+
 from . import module
 from . import common
 from . import io
