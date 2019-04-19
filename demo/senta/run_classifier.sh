163b7b19a1b6db5c840d92ac64baf548235c5fee
wuzewu <wuzewu@baidu.com>
2019-04-19 13:44:00 +0800

    add senta finetune code

---
+++ demo/senta/run_classifier.sh
@@ -0,0 +1,11 @@
+export CUDA_VISIBLE_DEVICES=0
+
+# User can select chnsenticorp, nlpcc_dbqa, lcqmc for different task
+DATASET="chnsenticorp"
+CKPT_DIR="./ckpt_${DATASET}"
+
+python -u text_classifier.py \
+                   --batch_size=24 \
+                   --use_gpu=True \
+                   --checkpoint_dir=${CKPT_DIR} \
+                   --num_epoch=3
