c5bf379ad1ed79b521c66e89da7031a6cc8f0985
Steffy-zxf <48793257+Steffy-zxf@users.noreply.github.com>
2019-06-04 14:38:55 +0800

    Update the demo text-cls and seq-label (#48)
    * Update the demo text-cls and seq-label

--- demo/sequence-labeling/predict.py
+++ demo/sequence-labeling/predict.py
@@ -93,21 +93,25 @@ if __name__ == '__main__':
         ["不过重在晋趣，略增明人气息，妙在集古有道、不露痕迹罢了。"],
     ]
 
-    results = seq_label_task.predict(data=data)[0]
-    np_infers = results[0]
-    np_lens = results[1]
+    results = seq_label_task.predict(data=data)
 
-    for index, text in enumerate(data):
-        labels = np_infers.reshape([-1]).astype(
-            np.int32).tolist()[args.max_seq_len * index:args.max_seq_len *
-                               (index + 1)]
-        label_str = ""
-        count = 0
-        for label_val in labels:
-            label_str += inv_label_map[label_val]
-            count += 1
-            if count == (np_lens[index]):
-                break
+    for num_batch, batch_results in enumerate(results):
+        infers = batch_results[0].reshape([-1]).astype(np.int32).tolist()
+        np_lens = batch_results[1]
 
-        # Drop the label results of CLS and SEP Token
-        print("%s\tpredict=%s" % (text[0], label_str[1:-1]))
+        for index, np_len in enumerate(np_lens):
+            labels = infers[index * args.max_seq_len:(index + 1) *
+                            args.max_seq_len]
+
+            label_str = ""
+            count = 0
+            for label_val in labels:
+                label_str += inv_label_map[label_val]
+                count += 1
+                if count == np_len:
+                    break
+
+            # Drop the label results of CLS and SEP Token
+            print(
+                "%s\tpredict=%s" %
+                (data[num_batch * args.batch_size + index][0], label_str[1:-1]))
