a271703a16be6ea4d9eb10e198014bb81738ce75
Steffy-zxf <48793257+Steffy-zxf@users.noreply.github.com>
2019-06-17 10:53:47 +0800

    Update the senta demo (#56)

--- demo/senta/senta_finetune.py
+++ demo/senta/senta_finetune.py
@@ -8,7 +8,7 @@ import paddlehub as hub
 # yapf: disable
 parser = argparse.ArgumentParser(__doc__)
 parser.add_argument("--num_epoch", type=int, default=3, help="Number of epoches for fine-tuning.")
-parser.add_argument("--use_gpu", type=ast.literal_eval, default=False, help="Whether use GPU for finetuning, input should be True or False")
+parser.add_argument("--use_gpu", type=ast.literal_eval, default=True, help="Whether use GPU for finetuning, input should be True or False")
 parser.add_argument("--checkpoint_dir", type=str, default=None, help="Directory to model checkpoint")
 parser.add_argument("--batch_size", type=int, default=32, help="Total examples' number in batch for training.")
 args = parser.parse_args()
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     module = hub.Module(name="senta_bilstm")
     inputs, outputs, program = module.context(trainable=True)
 
-    # Step2: Download dataset and use TextClassificationReader to read dataset
+    # Step2: Download dataset and use LACClassifyReader to read dataset
     dataset = hub.dataset.ChnSentiCorp()
 
     reader = hub.reader.LACClassifyReader(
@@ -27,13 +27,9 @@ if __name__ == '__main__':
 
     sent_feature = outputs["sentence_feature"]
 
-    # Define a classfication finetune task by PaddleHub's API
-    cls_task = hub.create_text_cls_task(
-        feature=sent_feature, num_classes=dataset.num_labels)
-
     # Setup feed list for data feeder
     # Must feed all the tensor of senta's module need
-    feed_list = [inputs["words"].name, cls_task.variable('label').name]
+    feed_list = [inputs["words"].name]
 
     strategy = hub.finetune.strategy.AdamWeightDecayStrategy(
         learning_rate=1e-4, weight_decay=0.01, warmup_proportion=0.05)
@@ -45,7 +41,14 @@ if __name__ == '__main__':
         checkpoint_dir=args.checkpoint_dir,
         strategy=strategy)
 
+    # Define a classfication finetune task by PaddleHub's API
+    cls_task = hub.TextClassifierTask(
+        data_reader=reader,
+        feature=sent_feature,
+        feed_list=feed_list,
+        num_classes=dataset.num_labels,
+        config=config)
+
     # Finetune and evaluate by PaddleHub's API
     # will finish training, evaluation, testing, save model automatically
-    hub.finetune_and_eval(
-        task=cls_task, data_reader=reader, feed_list=feed_list, config=config)
+    cls_task.finetune_and_eval()
