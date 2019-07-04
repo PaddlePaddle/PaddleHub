6e5843846097e3b73a485ac54fcdb049d9656901
cclauss <cclauss@me.com>
2019-07-04 08:46:46 +0200

    Travis CI: Streamline and use flake8 to find Python syntax errors and… (#68)
    * Travis CI: Streamline and use flake8 to find Python syntax errors and undefined names
    
    The __sudo:__ tag is deprecated in Travis CI and __Linux__ and __Python 3.6__ are defaults.
    
    * import numpy as np for line 72
    
    * noqa: Code uses version detection instead of feature detection
    
    https://docs.python.org/3/howto/pyporting.html#use-feature-detection-instead-of-version-detection

--- paddlehub/reader/tokenization.py
+++ paddlehub/reader/tokenization.py
@@ -37,7 +37,7 @@ def convert_to_unicode(text):
     elif six.PY2:
         if isinstance(text, str):
             return text.decode("utf-8", "ignore")
-        elif isinstance(text, unicode):
+        elif isinstance(text, unicode):  # noqa
             return text
         else:
             raise ValueError("Unsupported string type: %s" % (type(text)))
@@ -60,7 +60,7 @@ def printable_text(text):
     elif six.PY2:
         if isinstance(text, str):
             return text
-        elif isinstance(text, unicode):
+        elif isinstance(text, unicode):  # noqa
             return text.encode("utf-8")
         else:
             raise ValueError("Unsupported string type: %s" % (type(text)))
