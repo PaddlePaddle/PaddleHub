281575bd1506406fe0bd53b560b75f9b9a548cc0
wuzewu <wuzewu@baidu.com>
2019-07-12 17:40:58 +0800

    Fix test bug

--- tests/modules/test_senta.py
+++ tests/modules/test_senta.py
@@ -1,3 +1,4 @@
+#coding:utf-8
 import paddlehub as hub
 
 senta = hub.Module(name="senta_bilstm")
--- tests/modules/test_simnet.py
+++ tests/modules/test_simnet.py
@@ -1,3 +1,4 @@
+#coding:utf-8
 import paddlehub as hub
 
 simnet_bow = hub.Module(name="simnet_bow")
