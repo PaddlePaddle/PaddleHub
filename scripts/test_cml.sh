749e51917e8fa523289208ad7b46293c04bb6d29
wuzewu <wuzewu@baidu.com>
2019-07-12 16:10:54 +0800

    Update test script

--- scripts/test_cml.sh
+++ scripts/test_cml.sh
@@ -3,5 +3,34 @@ set -o errexit
 base_path=$(cd `dirname $0`; pwd)
 cd $base_path
 
+alias hub="python $base_path/../paddlehub/commands/hub.py "$@""
+
 # test install command
 hub install lac
+
+# test show command
+hub show lac
+
+# test list command
+hub list
+
+# test version command
+hub version
+
+# test help command
+hub help
+
+# test uninstall command
+hub uninstall lac
+
+# test clear command
+hub clear
+
+# test search command
+hub search lac
+
+# test run command
+hub run lac --input_text "今天天气不错"
+
+# test download command
+hub download lac
