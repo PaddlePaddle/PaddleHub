fd432bbd7b6bf3ac1ca8d8d30dec4bc7f7957f51
kinghuin <kinghuin_chull@163.com>
2019-11-01 15:09:35 +0800

    debug mrc task

--- paddlehub/finetune/task/reading_comprehension_task.py
+++ paddlehub/finetune/task/reading_comprehension_task.py
@@ -176,8 +176,10 @@ def write_predictions(all_examples, all_features, all_results, n_best_size,
     logger.info("Writing nbest to: %s" % (output_nbest_file))
 
     example_index_to_features = collections.defaultdict(list)
+    print(all_features[:3])
     for feature in all_features:
         example_index_to_features[feature.example_index].append(feature)
+    print(example_index_to_features.items()[:3])
 
     unique_id_to_result = {}
     for result in all_results:
