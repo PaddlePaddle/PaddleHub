23664e0b23a5e0a3ddb676e324bcd77b6767e4b1
kinghuin <kinghuin_chull@163.com>
2019-12-05 15:36:07 +0800

    update nlp_reader

--- paddlehub/reader/nlp_reader.py
+++ paddlehub/reader/nlp_reader.py
@@ -68,11 +68,12 @@ class BaseReader(Basic_Reader):
 
         # generate label map
         self.label_map = {}
-        if self.dataset:
+        try:
             for index, label in enumerate(self.dataset.get_labels()):
                 self.label_map[label] = index
             logger.info("Dataset label map = {}".format(self.label_map))
-        else:
+        except:
+            # some dataset like squad, its label_list=None
             logger.info(
                 "Dataset is None or it has not any labels, label map = {}".
                 format(self.label_map))
