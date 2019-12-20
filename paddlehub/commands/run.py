1d01b40137d36caf7285ce89682e44ca6922766e
wuzewu <wuzewu@baidu.com>
2019-12-20 14:10:37 +0800

    fix bug

--- paddlehub/commands/run.py
+++ paddlehub/commands/run.py
@@ -241,7 +241,7 @@ class RunCommand(BaseCommand):
             return False
 
         if self.module.code_version == "v2":
-            result = self.module(argv[1:])
+            results = self.module(argv[1:])
         else:
             self.module.check_processor()
             self.add_module_config_arg()
