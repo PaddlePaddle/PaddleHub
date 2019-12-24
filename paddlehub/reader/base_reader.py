a87cd24b0aa9ffb30e5ca466467938935f04646f
kinghuin <kinghuin_chull@163.com>
2019-12-24 20:07:35 +0800

    del current_example and current_epoch

--- paddlehub/reader/base_reader.py
+++ paddlehub/reader/base_reader.py
@@ -4,11 +4,8 @@ import numpy as np
 class BaseReader(object):
     def __init__(self, dataset, random_seed=None):
         self.dataset = dataset
-        np.random.seed(random_seed)
-
-        self.current_example = 0
-        self.current_epoch = 0
         self.num_examples = {'train': -1, 'dev': -1, 'test': -1}
+        np.random.seed(random_seed)
 
     def get_train_examples(self):
         return self.dataset.get_train_examples()
