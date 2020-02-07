14aaa919dc2a802e6b01062d5fe89c0ba017645c
Steffy-zxf <48793257+Steffy-zxf@users.noreply.github.com>
2020-02-07 20:04:55 +0800

    add requirements for readthedocs (#341)
    * add requirements for readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,7 @@
 from recommonmark.transform import AutoStructify
 from recommonmark.parser import CommonMarkParser
 import os
+import paddlehub as hub
 import sys
 sys.path.insert(0, os.path.abspath('../paddlehub'))
 
