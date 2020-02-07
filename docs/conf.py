d98c88a49d6e8e3b5c5612e2e6f9859eb917586d
zhangxuefei <zhangxuefei@baidu.com>
2020-02-07 19:31:43 +0800

    update docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, PaddlePaddle'
 author = 'PaddlePaddle'
 
 # The full version, including alpha/beta/rc tags
-release = "1.5.1"
+release = "v1.5"
 
 # -- General configuration ---------------------------------------------------
 
