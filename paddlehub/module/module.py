9bbc521e437aceba4b77259285df455d0ea4ab69
kinghuin <kinghuin_chull@163.com>
2020-03-06 17:32:47 +0800

    add todo

--- paddlehub/module/module.py
+++ paddlehub/module/module.py
@@ -181,6 +181,7 @@ class Module(object):
         dirname = os.path.join(*list(os.path.split(directory)[:-1]))
         sys.path.insert(0, dirname)
         _module = importlib.import_module("{}.module".format(basename))
+        # TODO: get real module
         for _item, _cls in inspect.getmembers(_module, inspect.isclass)[1:]:
             _item = _module.__dict__[_item]
             if issubclass(_item, Module):
