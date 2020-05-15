d6763ca281eb51c80d6196f2dc0f7a533a229d81
kinghuin <kinghuin_chull@163.com>
2020-05-15 23:37:27 +0800

    fix load_checkpoint bug (#600)

--- paddlehub/finetune/task/base_task.py
+++ paddlehub/finetune/task/base_task.py
@@ -833,8 +833,9 @@ class BaseTask(object):
             self.exe,
             main_program=self.main_program)
         # Revise max_train_steps when incremental training
-        self.max_train_steps = self.env.current_step + self.max_train_steps / self.config.num_epoch * (
-            self.config.num_epoch - self.env.current_epoch + 1)
+        if is_load_successful:
+            self.max_train_steps = self.env.current_step + self.max_train_steps / self.config.num_epoch * (
+                self.config.num_epoch - self.env.current_epoch + 1)
         return is_load_successful
 
     def load_parameters(self, dirname):
