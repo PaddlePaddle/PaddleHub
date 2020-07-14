6a874cdb77d934cf246a69be5c3ef43261ff2c61
Steffy-zxf <zhangxuefei@baidu.com>
2020-07-14 16:11:41 +0800

    rm tencent embedding modules

--- demo/pointwise_text_matching/run_ernie_pointwise_matching.sh
+++ demo/pointwise_text_matching/run_ernie_pointwise_matching.sh
@@ -1,12 +1,13 @@
+export CUDA_VISIBLE_DEVICES=0
 CKPT_DIR="./ckpt_ernie_pointwise_matching"
 
 python -u ernie_pointwise_matching.py \
             --batch_size=32 \
-            --use_gpu=False \
+            --use_gpu=True \
             --checkpoint_dir=${CKPT_DIR} \
             --learning_rate=5e-5 \
             --max_seq_len=128 \
             --num_epoch=3 \
             --warmup_proportion=0.1 \
             --weight_decay=0.01 \
-            --use_data_parallel=False \
+            --use_data_parallel=True \
