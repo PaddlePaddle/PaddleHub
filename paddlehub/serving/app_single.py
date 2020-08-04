dd17593cad1cf17e919600b7647e72b6b823f321
ShenYuhan <yhan_shen@163.com>
2020-08-04 17:41:43 +0800

    delete serving index.html

--- paddlehub/serving/app_single.py
+++ paddlehub/serving/app_single.py
@@ -321,7 +321,6 @@ def create_app(init_flag=False, configs=None):
         return '暂不提供可视化界面，请直接使用脚本进行请求。<br/>No visual ' \
                'interface is provided for the time being, please use the' \
                ' python script to make a request directly.'
-        return render_template("main.html")
 
     @app_instance.before_request
     def before_request():
