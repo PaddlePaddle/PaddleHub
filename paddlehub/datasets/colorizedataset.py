75b722aeaf39fce619ccae5a1d508a0bbd2f93f1
haoyuying <18844182690@163.com>
2020-09-14 14:20:22 +0800

    revise colorizedataset.py

--- paddlehub/datasets/colorizedataset.py
+++ paddlehub/datasets/colorizedataset.py
@@ -1,3 +1,18 @@
+# coding:utf-8
+# Copyright (c) 2020  PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
 
 import numpy
@@ -27,7 +42,7 @@ class Colorizedataset(paddle.io.Dataset):
         else:
             self.file = 'validation'
             
-        self.file = os.path.join(DATA_HOME, 'flower_photos', self.file)
+        self.file = os.path.join(DATA_HOME, 'canvas', self.file)
         self.data = get_img_file(self.file)
 
     def __getitem__(self, idx: int) -> numpy.ndarray:
