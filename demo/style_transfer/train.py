262c4a14b9eeaf63044be84bdafc355a375f1eff
haoyuying <18844182690@163.com>
2020-11-05 21:28:39 +0800

    revise style

--- demo/style_transfer/train.py
+++ demo/style_transfer/train.py
@@ -3,13 +3,13 @@ import paddlehub as hub
 
 from paddlehub.finetune.trainer import Trainer
 from paddlehub.datasets.minicoco import MiniCOCO
-from paddlehub.vision.transforms import Compose, Resize, CenterCrop
+import paddlehub.vision.transforms as T
 
 if __name__ == "__main__":
     model = hub.Module(name='msgnet')
-    transform = Compose([Resize((256, 256), interpolation='LINEAR')])
+    transform = T.Compose([T.Resize((256, 256), interpolation='LINEAR')])
     styledata = MiniCOCO(transform)
     scheduler =  paddle.optimizer.lr.PolynomialDecay(learning_rate=0.001, power=0.9, decay_steps=100)
     optimizer = paddle.optimizer.Adam(learning_rate=scheduler, parameters=model.parameters())
-    trainer = Trainer(model, optimizer, checkpoint_dir='test_ckpt_img')
+    trainer = Trainer(model, optimizer, checkpoint_dir='test_style_ckpt')
     trainer.train(styledata, epochs=101, batch_size=4, eval_dataset=styledata, log_interval=10, save_interval=10)
