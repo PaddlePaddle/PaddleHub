c4f19c8e927293ca057f0241c1a221c5726565c6
wuzewu <wuzewu@baidu.com>
2020-11-17 10:36:24 +0800

    Remove detection demo

--- demo/detection/yolov3_darknet53_pascalvoc/predict.py
+++
@@ -1,9 +0,0 @@
-import paddle
-import paddlehub as hub
-
-if __name__ == '__main__':
-    place = paddle.CUDAPlace(0)
-    paddle.disable_static()
-    model = hub.Module(name='yolov3_darknet53_pascalvoc', is_train=False)
-    model.eval()
-    model.predict(imgpath="4026.jpeg", filelist="/PATH/TO/JSON")
