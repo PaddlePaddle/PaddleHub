e0ddcc1d51fe351b8855c3e79582d231a9536ac5
jm12138 <2286040843@qq.com>
2020-12-10 00:18:30 +0800

    Add U2Net and U2Netp Module
    Add U2Net and U2Netp Module

---
+++ modules/image/semantic_segmentation/U2Netp/README.md
@@ -0,0 +1,56 @@
+## 概述
+* $U^2Net$的网络结构如下图，其类似于编码-解码(Encoder-Decoder)结构的 U-Net
+* 每个 stage 由新提出的 RSU模块(residual U-block) 组成. 例如，En_1 即为基于 RSU 构建的
+* $U^2Net^+$是一个小型化的$U^2Net$
+
+![](https://ai-studio-static-online.cdn.bcebos.com/999d37b4ffdd49dc9e3315b7cec7b2c6918fdd57c8594ced9dded758a497913d)
+
+## 效果展示
+![](https://ai-studio-static-online.cdn.bcebos.com/4d77bc3a05cf48bba6f67b797978f4cdf10f38288b9645d59393dd85cef58eff)
+![](https://ai-studio-static-online.cdn.bcebos.com/d7839c7207024747b32e42e49f7881bd2554d8408ab44e669fb340b50d4e38de)
+
+## API
+```python
+def Segmentation(
+        images=None,
+        paths=None,
+        batch_size=1,
+        input_size=320,
+        output_dir='output',
+        visualization=False):
+```
+图像前景背景分割 API
+
+**参数**
+* images (list[np.ndarray]) : 输入图像数据列表（BGR）
+* paths (list[str]) : 输入图像路径列表
+* batch_size (int) : 数据批大小
+* input_size (int) : 输入图像大小
+* output_dir (str) : 可视化图像输出目录
+* visualization (bool) : 是否可视化
+
+**返回**
+* results (list[np.ndarray]): 输出图像数据列表
+
+**代码示例**
+```python
+import cv2
+import paddlehub as hub
+
+model = hub.Module(name='U2Netp')
+
+result = model.Segmentation(
+    images=[cv2.imread('/PATH/TO/IMAGE')],
+    paths=None,
+    batch_size=1,
+    input_size=320,
+    output_dir='output',
+    visualization=True)
+```
+
+## 查看代码
+https://github.com/NathanUA/U-2-Net
+
+## 依赖
+paddlepaddle >= 2.0.0rc0  
+paddlehub >= 2.0.0b1
---
+++ modules/image/semantic_segmentation/U2Netp/processor.py
@@ -0,0 +1,96 @@
+import os
+import cv2
+import numpy as np
+
+__all__ = ['Processor']
+
+class Processor():
+    def __init__(self, paths, images, batch_size, input_size):
+        # 图像列表
+        self.imgs = self.load_datas(paths, images)
+
+        # 输入数据
+        self.input_datas = self.preprocess(self.imgs, batch_size, input_size)
+
+    # 读取数据函数
+    def load_datas(self, paths, images):
+        datas = []
+
+        # 读取数据列表
+        if paths is not None:
+            for im_path in paths:
+                assert os.path.isfile(im_path), "The {} isn't a valid file path.".format(im_path)
+                im = cv2.imread(im_path)
+                datas.append(im)
+
+        if images is not None:
+            datas = images
+        
+        # 返回数据列表
+        return datas
+
+    # 预处理
+    def preprocess(self, imgs, batch_size=1, input_size=320):
+        input_datas = []
+        for image in imgs:
+            # h, w = image.shape[:2]
+            # if h > w:
+            #     new_h, new_w = input_size*h/w,input_size
+            # else:
+            #     new_h, new_w = input_size,input_size*w/h
+            # image = cv2.resize(image, (int(new_w), int(new_h)))
+            
+            image = cv2.resize(image, (input_size, input_size))
+            tmpImg = np.zeros((image.shape[0],image.shape[1],3))
+            image = image/np.max(image)
+
+            tmpImg[:,:,0] = (image[:,:,0]-0.485)/0.229
+            tmpImg[:,:,1] = (image[:,:,1]-0.456)/0.224
+            tmpImg[:,:,2] = (image[:,:,2]-0.406)/0.225
+
+            # convert BGR to RGB
+            tmpImg = tmpImg.transpose((2, 0, 1))
+            tmpImg = tmpImg[np.newaxis,:,:,:]
+            input_datas.append(tmpImg)
+        
+        input_datas = np.concatenate(input_datas, 0)
+
+        datas_num = input_datas.shape[0]
+        split_num = datas_num//batch_size+1 if datas_num%batch_size!=0 else datas_num//batch_size
+
+        input_datas = np.array_split(input_datas, split_num)
+
+        return input_datas
+
+    def normPRED(self, d):
+        ma = np.max(d)
+        mi = np.min(d)
+
+        dn = (d-mi)/(ma-mi)
+
+        return dn
+
+    # 后处理
+    def postprocess(self, outputs, visualization=False, output_dir='output'):
+        results = []
+        if visualization and not os.path.exists(output_dir):
+            os.mkdir(output_dir)
+            
+        for i, image in enumerate(self.imgs):
+            # normalization
+            pred = 1.0 - outputs[i,0,:,:]
+
+            pred = self.normPRED(pred)
+
+            # convert torch tensor to numpy array
+            pred = pred.squeeze()
+            pred = (pred*255).astype(np.uint8)
+            h, w = image.shape[:2]
+            pred = cv2.resize(pred, (w, h))
+            
+            results.append(pred)
+
+            if visualization:
+                cv2.imwrite(os.path.join(output_dir, 'result_%d.png' % i), pred)
+
+        return results
