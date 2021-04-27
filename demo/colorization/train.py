1a26842dbca3eb21194f22dfd347856a593f4163
haoyuying <18844182690@163.com>
2021-04-27 14:18:30 +0800

    -

--- demo/colorization/train.py
+++ demo/colorization/train.py
@@ -11,11 +11,11 @@ if __name__ == '__main__':
                            T.RGB2LAB()], to_rgb=True)
 
     color_set = Canvas(transform=transform, mode='train')
-    model = hub.Module(name='user_guided_colorization')
+    model = hub.Module(name='user_guided_colorization', load_checkpoint='/PATH/TO/CHECKPOINT')
 
     model.set_config(classification=True, prob=1)
     optimizer = paddle.optimizer.Adam(learning_rate=0.0001, parameters=model.parameters())
-    trainer = Trainer(model, optimizer, checkpoint_dir='img_colorization_ckpt_cls_1', use_gpu=True)
+    trainer = Trainer(model, optimizer, checkpoint_dir='img_colorization_ckpt_cls_1')
     trainer.train(color_set, epochs=201, batch_size=25, eval_dataset=color_set, log_interval=10, save_interval=10)
 
     model.set_config(classification=False, prob=0.125)
