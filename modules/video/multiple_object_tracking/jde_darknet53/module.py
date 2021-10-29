deadba2cf9519197d06a639be5fc2616fd22d35e
chenjian <chenjian26@baidu.com>
2021-10-29 16:40:31 +0800

    Fix MOT module bug (#1665)

--- modules/video/multiple_object_tracking/jde_darknet53/module.py
+++ modules/video/multiple_object_tracking/jde_darknet53/module.py
@@ -31,12 +31,13 @@ from .tracker import StreamTracker
 logger = setup_logger('Predict')
 
 
-@moduleinfo(name="jde_darknet53",
-            type="CV/multiple_object_tracking",
-            author="paddlepaddle",
-            author_email="",
-            summary="JDE is a joint detection and appearance embedding model for multiple object tracking.",
-            version="1.0.0")
+@moduleinfo(
+    name="jde_darknet53",
+    type="CV/multiple_object_tracking",
+    author="paddlepaddle",
+    author_email="",
+    summary="JDE is a joint detection and appearance embedding model for multiple object tracking.",
+    version="1.0.0")
 class JDETracker_1088x608:
     def __init__(self):
         self.pretrained_model = os.path.join(self.directory, "jde_darknet53_30e_1088x608")
@@ -70,12 +71,13 @@ class JDETracker_1088x608:
         tracker.load_weights_jde(self.pretrained_model)
         signal.signal(signal.SIGINT, self.signalhandler)
         # inference
-        tracker.videostream_predict(video_stream=video_stream,
-                                    output_dir=output_dir,
-                                    data_type='mot',
-                                    model_type='JDE',
-                                    visualization=visualization,
-                                    draw_threshold=draw_threshold)
+        tracker.videostream_predict(
+            video_stream=video_stream,
+            output_dir=output_dir,
+            data_type='mot',
+            model_type='JDE',
+            visualization=visualization,
+            draw_threshold=draw_threshold)
 
     def stream_mode(self, output_dir='mot_result', visualization=True, draw_threshold=0.5, use_gpu=False):
         '''
@@ -106,11 +108,12 @@ class JDETracker_1088x608:
         return self
 
     def __enter__(self):
-        self.tracker_generator = self.tracker.imagestream_predict(self.output_dir,
-                                                                  data_type='mot',
-                                                                  model_type='JDE',
-                                                                  visualization=self.visualization,
-                                                                  draw_threshold=self.draw_threshold)
+        self.tracker_generator = self.tracker.imagestream_predict(
+            self.output_dir,
+            data_type='mot',
+            model_type='JDE',
+            visualization=self.visualization,
+            draw_threshold=self.draw_threshold)
         next(self.tracker_generator)
 
     def __exit__(self, exc_type, exc_value, traceback):
@@ -129,10 +132,12 @@ class JDETracker_1088x608:
                 logger.info('No output images to save for video')
                 return
             img = cv2.imread(os.path.join(save_dir, '00000.jpg'))
-            video_writer = cv2.VideoWriter(output_video_path,
-                                           fourcc=cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'),
-                                           fps=30,
-                                           frameSize=[img.shape[1], img.shape[0]])
+            video_writer = cv2.VideoWriter(
+                output_video_path,
+                apiPreference=0,
+                fourcc=cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'),
+                fps=30,
+                frameSize=(img.shape[1], img.shape[0]))
             for i in range(len(imgnames)):
                 imgpath = os.path.join(save_dir, '{:05d}.jpg'.format(i))
                 img = cv2.imread(imgpath)
@@ -169,10 +174,11 @@ class JDETracker_1088x608:
         """
         Run as a command.
         """
-        self.parser = argparse.ArgumentParser(description="Run the {} module.".format(self.name),
-                                              prog='hub run {}'.format(self.name),
-                                              usage='%(prog)s',
-                                              add_help=True)
+        self.parser = argparse.ArgumentParser(
+            description="Run the {} module.".format(self.name),
+            prog='hub run {}'.format(self.name),
+            usage='%(prog)s',
+            add_help=True)
 
         self.arg_input_group = self.parser.add_argument_group(title="Input options", description="Input data. Required")
         self.arg_config_group = self.parser.add_argument_group(
@@ -204,10 +210,12 @@ class JDETracker_1088x608:
                 logger.info('No output images to save for video')
                 return
             img = cv2.imread(os.path.join(save_dir, '00000.jpg'))
-            video_writer = cv2.VideoWriter(output_video_path,
-                                           fourcc=cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'),
-                                           fps=30,
-                                           frameSize=[img.shape[1], img.shape[0]])
+            video_writer = cv2.VideoWriter(
+                output_video_path,
+                apiPreference=0,
+                fourcc=cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'),
+                fps=30,
+                frameSize=(img.shape[1], img.shape[0]))
             for i in range(len(imgnames)):
                 imgpath = os.path.join(save_dir, '{:05d}.jpg'.format(i))
                 img = cv2.imread(imgpath)
@@ -223,22 +231,16 @@ class JDETracker_1088x608:
         """
         self.arg_config_group.add_argument('--use_gpu', action='store_true', help="use GPU or not")
 
-        self.arg_config_group.add_argument('--output_dir',
-                                           type=str,
-                                           default='mot_result',
-                                           help='Directory name for output tracking results.')
-        self.arg_config_group.add_argument('--visualization',
-                                           action='store_true',
-                                           help="whether to save output as images.")
-        self.arg_config_group.add_argument("--draw_threshold",
-                                           type=float,
-                                           default=0.5,
-                                           help="Threshold to reserve the result for visualization.")
+        self.arg_config_group.add_argument(
+            '--output_dir', type=str, default='mot_result', help='Directory name for output tracking results.')
+        self.arg_config_group.add_argument(
+            '--visualization', action='store_true', help="whether to save output as images.")
+        self.arg_config_group.add_argument(
+            "--draw_threshold", type=float, default=0.5, help="Threshold to reserve the result for visualization.")
 
     def add_module_input_arg(self):
         """
         Add the command input options.
         """
-        self.arg_input_group.add_argument('--video_stream',
-                                          type=str,
-                                          help="path to video stream, can be a video file or stream device number.")
+        self.arg_input_group.add_argument(
+            '--video_stream', type=str, help="path to video stream, can be a video file or stream device number.")
