55988a651ca2c0369186bcbd2700b813a178460b
chenjian <chenjian26@baidu.com>
2022-03-08 17:42:29 +0800

    fix according to review

--- modules/image/image_processing/prnet/utils/render_app.py
+++ modules/image/image_processing/prnet/utils/render_app.py
@@ -1,6 +1,20 @@
+# Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import numpy as np
-from utils.render import vis_of_vertices, render_texture
 from scipy import ndimage
+from utils.render import render_texture
+from utils.render import vis_of_vertices
 
 
 def get_visibility(vertices, triangles, h, w):
