ae9058251d4987b05fea81388bc6a1ccf3eed6cc
jm12138 <2286040843@qq.com>
2022-08-17 16:39:09 +0800

    rm text_summarization

--- modules/text/text_generation/ernie_zeus/module.py
+++ modules/text/text_generation/ernie_zeus/module.py
@@ -397,49 +397,49 @@ class ERNIEZeus:
 
 
 if __name__ == '__main__':
-    ernie3_zeus = ERNIEZeus()
+    ernie_zeus = ERNIEZeus()
 
-    result = ernie3_zeus.custom_generation(
+    result = ernie_zeus.custom_generation(
         '你好，'
     )
     print(result)
 
-    result = ernie3_zeus.text_generation(
+    result = ernie_zeus.text_generation(
         '给宠物猫起一些可爱的名字。名字：'
     )
     print(result)
 
-    result = ernie3_zeus.text_summarization(
+    result = ernie_zeus.text_summarization(
         '在芬兰、瑞典提交“入约”申请近一个月来，北约成员国内部尚未对此达成一致意见。与此同时，俄罗斯方面也多次对北约“第六轮扩张”发出警告。据北约官网显示，北约秘书长斯托尔滕贝格将于本月12日至13日出访瑞典和芬兰，并将分别与两国领导人进行会晤。'
     )
     print(result)
 
-    result = ernie3_zeus.copywriting_generation(
+    result = ernie_zeus.copywriting_generation(
         '芍药香氛的沐浴乳'
     )
     print(result)
 
-    result = ernie3_zeus.noval_continuation(
+    result = ernie_zeus.noval_continuation(
         '昆仑山可以说是天下龙脉的根源，所有的山脉都可以看作是昆仑的分支。这些分出来的枝枝杈杈，都可以看作是一条条独立的龙脉。'
     )
     print(result)
 
-    result = ernie3_zeus.answer_generation(
+    result = ernie_zeus.answer_generation(
         '交朋友的原则是什么？'
     )
     print(result)
 
-    result = ernie3_zeus.couplet_continuation(
+    result = ernie_zeus.couplet_continuation(
         '五湖四海皆春色'
     )
     print(result)
 
-    result = ernie3_zeus.composition_generation(
+    result = ernie_zeus.composition_generation(
         '诚以养德，信以修身'
     )
     print(result)
 
-    result = ernie3_zeus.text_cloze(
+    result = ernie_zeus.text_cloze(
         '她有着一双[MASK]的眼眸。'
     )
     print(result)
