3ed5ba2afbd27dbff610d032dfb63ff03d34b320
jm12138 <2286040843@qq.com>
2022-08-25 15:07:08 +0800

    update unittest

--- modules/image/object_detection/faster_rcnn_resnet50_coco2017/test.py
+++ modules/image/object_detection/faster_rcnn_resnet50_coco2017/test.py
@@ -12,9 +12,21 @@ class TestHubModule(unittest.TestCase):
             paths=['test.jpg'],
             visualization=False
         )
+        bbox = results[0]['data'][0]
+        label = bbox['label']
+        confidence = bbox['confidence']
+        left = bbox['left']
+        right = bbox['right']
+        top = bbox['top']
+        bottom = bbox['bottom']
 
-        data = results[0]['data']
-        self.assertEqual(len(data), 1)
+        self.assertEqual(label, 'cat')
+        self.assertTrue(confidence > 0.5)
+        self.assertTrue(450 < left < 550)
+        self.assertTrue(2850 < right < 2950)
+        self.assertTrue(750 < top < 850)
+        self.assertTrue(4000 < bottom < 4100)
+
 
 if __name__ == "__main__":
     unittest.main()
