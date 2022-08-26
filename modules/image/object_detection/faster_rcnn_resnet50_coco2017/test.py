fb3eef842a57fd7c9afbc14342692f725215188b
jm12138 <2286040843@qq.com>
2022-08-26 11:52:22 +0800

    update unittest

--- modules/image/object_detection/faster_rcnn_resnet50_coco2017/test.py
+++ modules/image/object_detection/faster_rcnn_resnet50_coco2017/test.py
@@ -32,10 +32,10 @@ class TestHubModule(unittest.TestCase):
 
         self.assertEqual(label, 'cat')
         self.assertTrue(confidence > 0.5)
-        self.assertTrue(450 < left < 550)
-        self.assertTrue(2850 < right < 2950)
-        self.assertTrue(750 < top < 850)
-        self.assertTrue(4000 < bottom < 4100)
+        self.assertTrue(200 < left < 800)
+        self.assertTrue(2500 < right < 3500)
+        self.assertTrue(500 < top < 1500)
+        self.assertTrue(3500 < bottom < 4500)
 
     def test_object_detection2(self):
         results = self.module.object_detection(
@@ -51,13 +51,48 @@ class TestHubModule(unittest.TestCase):
 
         self.assertEqual(label, 'cat')
         self.assertTrue(confidence > 0.5)
-        self.assertTrue(450 < left < 550)
-        self.assertTrue(2850 < right < 2950)
-        self.assertTrue(750 < top < 850)
-        self.assertTrue(4000 < bottom < 4100)
+        self.assertTrue(200 < left < 800)
+        self.assertTrue(2500 < right < 3500)
+        self.assertTrue(500 < top < 1500)
+        self.assertTrue(3500 < bottom < 4500)
+
+    def test_object_detection3(self):
+        results = self.module.object_detection(
+            images=[cv2.imread('tests/test.jpg')],
+            visualization=False
+        )
+        bbox = results[0]['data'][0]
+        label = bbox['label']
+        confidence = bbox['confidence']
+        left = bbox['left']
+        right = bbox['right']
+        top = bbox['top']
+        bottom = bbox['bottom']
+
+        self.assertEqual(label, 'cat')
+        self.assertTrue(confidence > 0.5)
+        self.assertTrue(200 < left < 800)
+        self.assertTrue(2500 < right < 3500)
+        self.assertTrue(500 < top < 1500)
+        self.assertTrue(3500 < bottom < 4500)
+
+    def test_object_detection4(self):
+        self.assertRaises(
+            cv2.error,
+            self.module.object_detection,
+            images=['no.jpg']
+        )
+
+    def test_object_detection5(self):
+        self.assertRaises(
+            cv2.error,
+            self.module.object_detection,
+            images='test.jpg'
+        )
 
     def test_save_inference_model(self):
         self.module.save_inference_model('./inference/model')
+
         self.assertTrue(os.path.exists('./inference/model.pdmodel'))
         self.assertTrue(os.path.exists('./inference/model.pdiparams'))
         self.assertTrue(os.path.exists('./inference/model.pdiparams.info'))
