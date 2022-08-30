6d907e96a03e4dbcf908d7613f4509f652bc38a2
jm12138 <2286040843@qq.com>
2022-08-30 15:12:43 +0800

    update hand_pose_localization

--- modules/image/keypoint_detection/hand_pose_localization/module.py
+++ modules/image/keypoint_detection/hand_pose_localization/module.py
@@ -1,11 +1,11 @@
 # coding=utf-8
 import os
 
-from paddlehub import Module
+import numpy as np
 from paddlehub.module.module import moduleinfo, serving
 
-from hand_pose_localization.model import Model
-from hand_pose_localization.processor import base64_to_cv2, Processor
+from .model import InferenceModel
+from .processor import base64_to_cv2, Processor
 
 
 @moduleinfo(
@@ -16,14 +16,16 @@ from hand_pose_localization.processor import base64_to_cv2, Processor
     summary="hand_pose_localization",  # 模型介绍
     version="1.0.2"  # 版本号
 )
-class Hand_Pose_Localization(Module):
+class Hand_Pose_Localization:
     # 初始化函数
-    def __init__(self, name=None, use_gpu=False):
+    def __init__(self, use_gpu=False):
         # 设置模型路径
-        self.model_path = os.path.join(self.directory, "hand_pose_localization")
+        self.model_path = os.path.join(self.directory, "hand_pose_localization", "model")
 
         # 加载模型
-        self.model = Model(modelpath=self.model_path, use_gpu=use_gpu, use_mkldnn=False, combined=True)
+        self.model = InferenceModel(modelpath=self.model_path, use_gpu=use_gpu)
+
+        self.model.eval()
 
     # 关键点检测函数
     def keypoint_detection(self, images=None, paths=None, batch_size=1, output_dir='output', visualization=False):
@@ -31,7 +33,11 @@ class Hand_Pose_Localization(Module):
         processor = Processor(images, paths, batch_size, output_dir)
 
         # 模型预测
-        outputs = self.model.predict(processor.input_datas)
+        outputs = []
+        for input_data in processor.input_datas:
+            output = self.model(input_data)
+            outputs.append(output)
+        outputs = np.concatenate(outputs, 0)
 
         # 结果后处理
         results = processor.postprocess(outputs, visualization)
---
+++ modules/image/keypoint_detection/hand_pose_localization/test.py
@@ -0,0 +1,73 @@
+import os
+import unittest
+
+import cv2
+import requests
+import paddlehub as hub
+
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+
+
+class TestHubModule(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        img_url = 'https://unsplash.com/photos/8UAUuP97RlY/download?ixid=MnwxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjYxODQxMzI1&force=true'
+        if not os.path.exists('tests'):
+            os.makedirs('tests')
+        response = requests.get(img_url)
+        assert response.status_code == 200, 'Network Error.'
+        with open('tests/test.jpg', 'wb') as f:
+            f.write(response.content)
+        cls.module = hub.Module(name="hand_pose_localization")
+
+    def test_keypoint_detection1(self):
+        results = self.module.keypoint_detection(
+            paths=['tests/test.jpg'],
+            visualization=False
+        )
+        kps = results[0]
+        self.assertIsInstance(kps, list)
+
+    def test_keypoint_detection2(self):
+        results = self.module.keypoint_detection(
+            images=[cv2.imread('tests/test.jpg')],
+            visualization=False
+        )
+        kps = results[0]
+        self.assertIsInstance(kps, list)
+
+    def test_keypoint_detection3(self):
+        results = self.module.keypoint_detection(
+            images=[cv2.imread('tests/test.jpg')],
+            visualization=True
+        )
+        kps = results[0]
+        self.assertIsInstance(kps, list)
+
+    def test_keypoint_detection4(self):
+        self.module = hub.Module(name="hand_pose_localization", use_gpu=True)
+        results = self.module.keypoint_detection(
+            images=[cv2.imread('tests/test.jpg')],
+            visualization=False
+        )
+        kps = results[0]
+        self.assertIsInstance(kps, list)
+
+    def test_keypoint_detection5(self):
+        self.assertRaises(
+            AssertionError,
+            self.module.keypoint_detection,
+            paths=['no.jpg']
+        )
+
+    def test_keypoint_detection6(self):
+        self.assertRaises(
+            AttributeError,
+            self.module.keypoint_detection,
+            images=['test.jpg']
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
