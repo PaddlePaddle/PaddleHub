a53abfa8015f071d9e8328f3e20459fa1b076eb9
chenjian <chenjian26@baidu.com>
2022-09-29 08:48:21 +0000

    add stable_diffusion_img2img module

---
+++ modules/image/text_to_image/stable_diffusion_img2img/utils.py
@@ -0,0 +1,16 @@
+import numpy as np
+import paddle
+import PIL
+from PIL import Image
+
+
+def preprocess(image):
+    if isinstance(image, np.ndarray):
+        image = Image.fromarray(image)
+    w, h = image.size
+    w, h = map(lambda x: x - x % 32, (w, h))  # resize to integer multiple of 32
+    image = image.resize((w, h), resample=PIL.Image.LANCZOS)
+    image = np.array(image).astype(np.float32) / 255.0
+    image = image[None].transpose(0, 3, 1, 2)
+    image = paddle.to_tensor(image)
+    return 2.0 * image - 1.0
