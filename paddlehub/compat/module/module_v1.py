a5b4c89ad0eed7b1f8f5bfc08713fa94fecb8d52
chenjian <chenjian26@baidu.com>
2022-11-09 08:53:25 +0000

    fix

--- paddlehub/compat/module/module_v1.py
+++ paddlehub/compat/module/module_v1.py
@@ -179,16 +179,20 @@ class ModuleV1(object):
                 for item in zip(*process_data):
                     yield item
 
+            nonlocal feed_dict
             process_data = []
             feed_name_list = []
+            feed_list = []
+            print(feed_dict)
+            print(data_format)
             for key in data_format:
                 process_data.append([value['processed'] for value in data[key]])
                 feed_name_list.append(data_format[key]['feed_key'])
-            loader = paddle.io.DataLoader.from_generator(feed_list=feed_name_list)
-            loader.set_sample_list_generator(_reader, places=place)
+                feed_list.append(feed_dict[key])
+            loader = paddle.io.DataLoader.from_generator(feed_list=feed_list, capacity=1)
             return functools.partial(_reader, process_data=process_data), loader
 
-        _, fetch_dict, program = self.context(signature=sign_name, for_test=True)
+        feed_dict, fetch_dict, program = self.context(signature=sign_name, for_test=True)
         fetch_list = list([value for key, value in fetch_dict.items()])
         with paddle.static.program_guard(program):
             result = []
@@ -200,6 +204,7 @@ class ModuleV1(object):
             data_format = self.processor.data_format(sign_name=sign_name)
             reader, loader = _get_reader_and_feeder(data_format, data, place)
             reader = paddle.batch(reader, batch_size=batch_size)
+            loader.set_sample_list_generator(reader, places=place)
             for batch in loader():
                 data_out = exe.run(feed=batch, fetch_list=fetch_list, return_numpy=False)
                 sub_data = {key: value[index:index + len(batch)] for key, value in data.items()}
