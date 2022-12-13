30e5d92a18017e2f3360a69e4f3a4bcb55b65436
jm12138 <2286040843@qq.com>
2022-12-13 16:51:47 +0800

    update falsr_c

--- modules/image/Image_editing/super_resolution/falsr_c/test.py
+++ modules/image/Image_editing/super_resolution/falsr_c/test.py
@@ -3,18 +3,19 @@ import shutil
 import unittest
 
 import cv2
-import requests
 import numpy as np
-import paddlehub as hub
+import requests
 
+import paddlehub as hub
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
 
 class TestHubModule(unittest.TestCase):
+
     @classmethod
     def setUpClass(cls) -> None:
-        img_url = 'https://unsplash.com/photos/1sLIu1XKQrY/download?ixid=MnwxMjA3fDB8MXxhbGx8MTJ8fHx8fHwyfHwxNjYyMzQxNDUx&force=true&w=640'
+        img_url = 'https://unsplash.com/photos/1sLIu1XKQrY/download?ixid=MnwxMjA3fDB8MXxhbGx8MTJ8fHx8fHwyfHwxNjYyMzQxNDUx&force=true&w=120'
         if not os.path.exists('tests'):
             os.makedirs('tests')
         response = requests.get(img_url)
@@ -30,50 +31,26 @@ class TestHubModule(unittest.TestCase):
         shutil.rmtree('falsr_c_output')
 
     def test_reconstruct1(self):
-        results = self.module.reconstruct(
-            paths=['tests/test.jpg'],
-            use_gpu=False,
-            visualization=False
-        )
+        results = self.module.reconstruct(paths=['tests/test.jpg'], use_gpu=False, visualization=False)
         self.assertIsInstance(results[0]['data'], np.ndarray)
 
     def test_reconstruct2(self):
-        results = self.module.reconstruct(
-            images=[cv2.imread('tests/test.jpg')],
-            use_gpu=False,
-            visualization=False
-        )
+        results = self.module.reconstruct(images=[cv2.imread('tests/test.jpg')], use_gpu=False, visualization=False)
         self.assertIsInstance(results[0]['data'], np.ndarray)
 
     def test_reconstruct3(self):
-        results = self.module.reconstruct(
-            images=[cv2.imread('tests/test.jpg')],
-            use_gpu=False,
-            visualization=True
-        )
+        results = self.module.reconstruct(images=[cv2.imread('tests/test.jpg')], use_gpu=False, visualization=True)
         self.assertIsInstance(results[0]['data'], np.ndarray)
 
     def test_reconstruct4(self):
-        results = self.module.reconstruct(
-            images=[cv2.imread('tests/test.jpg')],
-            use_gpu=True,
-            visualization=False
-        )
+        results = self.module.reconstruct(images=[cv2.imread('tests/test.jpg')], use_gpu=True, visualization=False)
         self.assertIsInstance(results[0]['data'], np.ndarray)
 
     def test_reconstruct5(self):
-        self.assertRaises(
-            AssertionError,
-            self.module.reconstruct,
-            paths=['no.jpg']
-        )
+        self.assertRaises(AssertionError, self.module.reconstruct, paths=['no.jpg'])
 
     def test_reconstruct6(self):
-        self.assertRaises(
-            AttributeError,
-            self.module.reconstruct,
-            images=['test.jpg']
-        )
+        self.assertRaises(AttributeError, self.module.reconstruct, images=['test.jpg'])
 
     def test_save_inference_model(self):
         self.module.save_inference_model('./inference/model')
