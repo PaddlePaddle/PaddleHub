ccbdff3b88a81b96ccfab3ed176f17c51f09d27a
chenjian <chenjian26@baidu.com>
2023-09-20 18:26:42 +0800

    bump paddlehub to v2.4.0

--- paddlehub/__init__.py
+++ paddlehub/__init__.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = 'develop'
+__version__ = '2.4.0'
 
 import paddle
 from packaging.version import Version
